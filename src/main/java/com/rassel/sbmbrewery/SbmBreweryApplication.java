0ac5fb6953aabb88b012e28583d98162d428f6f9
Rezaul Karim <rasselict07@gmail.com>
2021-02-11 00:30:47 +0900

    initial structure by spring initializer

---
+++ src/main/java/com/rassel/sbmbrewery/SbmBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.rassel.sbmbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmBreweryApplication.class, args);
+	}
+
+}
